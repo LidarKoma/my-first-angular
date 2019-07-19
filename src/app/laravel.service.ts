8e301f4c737e1baea1c93a70a27300ece5b4cadc
LidarKoma <lidarkoma@yahoo.com>
2019-07-18 23:53:08 -0500

    Update laravel.service.ts

--- src/app/laravel.service.ts
+++ src/app/laravel.service.ts
@@ -12,17 +12,17 @@ id:number;
   constructor(private http: HttpClient) { }
   getServices(): Observable<any[]> {
     //locally
-    return this.http.get<any[]>('http://localhost:8000/api/categories/')
+   // return this.http.get<any[]>('http://localhost:8000/api/categories/')
 
     //heroku
-    //return this.http.get<any[]>('https://book-reviws-api.herokuapp.com/api/categories/')
+    return this.http.get<any[]>('https://book-reviws-api.herokuapp.com/api/categories/')
   }
   getService(id:number): Observable<any> {
     //locally
-   return this.http.get('http://localhost:8000/api/categories/'+ id)
+   //return this.http.get('http://localhost:8000/api/categories/'+ id)
 
    //heroku
-    //return this.http.get('https://book-reviws-api.herokuapp.com/api/categories/'+ id)
+    return this.http.get('https://book-reviws-api.herokuapp.com/api/categories/'+ id)
 
   }
   setter(id:number){
